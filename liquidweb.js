f4f1a95c696d175671f8f84082195e635f8c653a
TechLxrd <baloggabriel76@outlook.com>
2025-11-30 14:26:22 +0200

    Add files via upload

--- liquidweb.js
+++ liquidweb.js
@@ -3,33 +3,27 @@
 
     const svgNS = "http://www.w3.org/2000/svg";
     const svg = document.createElementNS(svgNS, "svg");
-
     
     svg.style.position = "absolute";
-    svg.style.top = "-9999px"; 
-    svg.style.left = "-9999px";
+    svg.style.top = "-10000px";
+    svg.style.left = "-10000px";
     svg.style.width = "1px";
     svg.style.height = "1px";
     svg.style.overflow = "hidden";
-    
-    
-    svg.style.visibility = "hidden"; 
 
     svg.innerHTML = `
-      <filter id="liquid-filter" x="-20%" y="-20%" width="140%" height="140%" 
+      <filter id="liquid-filter" x="-50%" y="-50%" width="200%" height="200%" 
               filterUnits="objectBoundingBox" 
               primitiveUnits="userSpaceOnUse" 
               color-interpolation-filters="sRGB">
 
         <feTurbulence type="turbulence" baseFrequency="0.01 0.02" numOctaves="2" seed="2" result="turbulence" stitchTiles="noStitch" />
 
-        <feGaussianBlur stdDeviation="2" in="SourceGraphic" result="blur" color-interpolation-filters="sRGB" /> 
+        <feGaussianBlur stdDeviation="2" in="SourceGraphic" result="blur" /> 
 
-        <feDisplacementMap in="blur" in2="turbulence" scale="20" xChannelSelector="R" yChannelSelector="G" result="distort" />
-
-        <feComposite operator="in" in="distort" in2="SourceGraphic" result="final" />
-
-      </filter>
+        <feDisplacementMap in="blur" in2="turbulence" scale="20" xChannelSelector="R" yChannelSelector="G" result="final" />
+        
+        </filter>
     `;
 
     document.body.appendChild(svg);
